ec8bf628577ae70ec855a06125703d6213c0a17d
tajeru <tajeru47@gmail.com>
2024-10-04 14:51:30 +0900

    add CreatvieTab,example item(flourite)

---
+++ src/main/java/com/github/tajeru/parthenoncraft/item/ModItemGroups.java
@@ -0,0 +1,49 @@
+package com.github.tajeru.parthenoncraft.item;
+
+import com.github.tajeru.parthenoncraft.Parthenoncraft;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+
+
+    //クリエイティブタブの追加　アイコンの画像やタブの名前の定義 keyは翻訳で使用。
+        //このタグではフローライトをアイコンとしたタブを追加
+    public static final ItemGroup FLUORITE_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Parthenoncraft.MOD_ID, "fluorite"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.fluorite"))
+                    .icon(() -> new ItemStack(ModItems.FLUORITE)).entries((displayContext, entries) -> {
+                        //ここにタブ内に表示するアイテムを列挙
+                    entries.add(ModItems.FLUORITE);
+                    entries.add(ModItems.ROW_FLUORITE);
+
+
+                    }).build());
+
+
+    public static final ItemGroup PARTHENONCRAFT = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Parthenoncraft.MOD_ID, "fluorite_blocks"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.parthenoncraft"))
+                    .icon(() -> new ItemStack(ModItems.ROW_FLUORITE)).entries((displayContext, entries) -> {
+                        //ここにタブ内に表示するアイテムを列挙
+                        entries.add(Blocks.QUARTZ_PILLAR);
+
+
+                    }).build());
+
+
+
+    //main classと関係付け
+    public static void registerItemGroups(){
+        Parthenoncraft.LOGGER.info("Registering Item Groups for " + Parthenoncraft.MOD_ID);
+    }
+
+}
