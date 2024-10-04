3d8152cad3af3791ef50bc691a2c348295deba45
tajeru <tajeru47@gmail.com>
2024-10-04 11:15:13 +0900

    create directry first

--- src/main/java/com/github/tajeru/parthenoncraft/Parthenoncraft.java
+++ src/main/java/com/github/tajeru/parthenoncraft/Parthenoncraft.java
@@ -1,8 +1,13 @@
 package com.github.tajeru.parthenoncraft;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Parthenoncraft implements ModInitializer {
+    public static final String MOD_ID = "parthenoncraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
     @Override
     public void onInitialize() {
