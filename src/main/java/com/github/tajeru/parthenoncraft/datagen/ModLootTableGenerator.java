89e8e3b7a71d08433341b9125ef0fe87fb42d1c7
tajeru <tajeru47@gmail.com>
2024-10-05 06:47:18 +0900

    add dataGenerator logic

---
+++ src/main/java/com/github/tajeru/parthenoncraft/datagen/ModLootTableGenerator.java
@@ -0,0 +1,20 @@
+package com.github.tajeru.parthenoncraft.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+//dropItemとかのjsonファイルを設定してくれるらしい
+public class ModLootTableGenerator extends FabricTagProvider.BlockTagProvider {
+
+    public ModLootTableGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+
+    }
+}
